8a21da5da6bd9b2c29da6bc7d5f81517a24b91cf
trojan <patryk.trojanowski98@gmail.com>
2019-03-05 18:57:13 +0100

    rootReducer and rootActions setup

--- frontend/src/actions/rootActions.js
+++ frontend/src/actions/rootActions.js
@@ -1,3 +1,24 @@
+import { POSTING_FAILED, POSTING_FORM, POSTING_SUCCESS } from './actionTypes';
+
 export const postEventForm = values => {
   console.log(values);
+};
+
+export const postingForm = () => {
+  return {
+    type: POSTING_FORM
+  };
+};
+
+export const postingSuccess = () => {
+  return {
+    type: POSTING_SUCCESS
+  };
+};
+
+export const postingFailed = err => {
+  return {
+    type: POSTING_FAILED,
+    payload: err
+  };
 };
