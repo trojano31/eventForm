4cd83061a527b7e2c09fd8a9586d74f515be4530
trojano31 <patryk.trojanowski98@gmail.com>
2019-03-05 10:46:06 +0100

    initial frontend setup

---
+++ frontend/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './assets/styles/main.scss';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('index')
+);
---
+++ frontend/webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const SRC_DIR = __dirname + '/src';
+const DIST_DIR = __dirname + '/dist';
+
+module.exports = {
+  entry: [
+    SRC_DIR + '/index.jsx',
+  ],
+  output: {
+    path: DIST_DIR,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(html)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'html-loader',
+          options: { minimize: true },
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: SRC_DIR + '/index.html',
+      filename: './index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: DIST_DIR,
+    hot: true,
+    port: 3000,
+  },
+};
