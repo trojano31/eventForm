b0c18a3ffcf79eb42d2c57a86d4951159a81969a
trojano31 <patryk.trojanowski98@gmail.com>
2019-03-06 11:24:33 +0100

    test scripts in root package.json

--- backend/tests/event.test.js
+++ backend/tests/event.test.js
@@ -6,17 +6,19 @@ const clearEvents = async () => await Event.deleteMany({}, err => {
   if (err) throw err;
 });
 
-test('Should register a new event application', async () => {
-  clearEvents();
+test('Should register a new event application', async (done) => {
+  await clearEvents();
   await request(app).post('/events').send({
     firstName: 'Patryk',
     secondName: 'trojan',
     email: 'pat@gmail.com',
     date: '2019-03-06T09:42:58.602Z!'
-  }).expect(201)
+  }).expect(201);
+  done();
 });
 
 
-test('Should return list of event applications', async () => {
-  await request(app).get('/events').send().expect(200)
+test('Should return list of event applications', async (done) => {
+  await request(app).get('/events').send().expect(200);
+  done();
 });
