dbc5b992118d3785144ce824e7be431e2a10f28d
trojan <patryk.trojanowski98@gmail.com>
2019-05-08 11:53:46 +0200

    dockerize

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -4,6 +4,7 @@ mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false
-}, () => {
+}, (err) => {
+  if (err) return console.log(err);
   console.log("Database connected")
 });
